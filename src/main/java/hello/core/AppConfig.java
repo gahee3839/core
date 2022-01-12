216a44ce48e246fb71e6c05cbc8c811f92d64a27
gahee <gahee3839@gmail.com>
2022-01-12 17:56:00 +0900

    appConfig클래스를 스프링컨테이너로 대체 - @Configuration - @Bean

--- src/main/java/hello/core/AppConfig.java
+++ src/main/java/hello/core/AppConfig.java
@@ -8,21 +8,24 @@ import hello.core.member.MemberServiceImpl;
 import hello.core.member.MemoryMemberRepository;
 import hello.core.order.OrderService;
 import hello.core.order.OrderServiceImpl;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 
+@Configuration
 public class AppConfig {    //구현객체를 여기서 생성해서 주입해줌
-
+    @Bean
     public MemberService memberService() {
         return new MemberServiceImpl(memberRepository());
     }
-
-    private MemoryMemberRepository memberRepository() {
+    @Bean
+    public MemoryMemberRepository memberRepository() {
         return new MemoryMemberRepository();
     }
-
+    @Bean
     public OrderService orderService(){
         return new OrderServiceImpl(memberRepository(), discountPolicy());
     }
-
+    @Bean
     public DiscountPolicy discountPolicy() {
         return new RateDiscountPolicy();
     }

dd26eb1da22b890b6d5527d7aaccb9f7b487e59c
gahee <gahee3839@gmail.com>
2022-01-13 17:06:21 +0900

    싱글톤패턴의 문제 해결 (지역변수로 설정)

--- src/test/java/hello/core/singleton/StatefulServiceTest.java
+++ src/test/java/hello/core/singleton/StatefulServiceTest.java
@@ -15,15 +15,15 @@ public class StatefulServiceTest {
         StatefulService statefulService2 = ac.getBean(StatefulService.class);
 
         //ThreadA: A사용자 10000원주문
-        statefulService1.order("userA", 10000);
+        int userAPrice = statefulService1.order("userA", 10000);
         //ThreadB: B사용자 20000원주문
-        statefulService2.order("userB", 20000);
+        int userBPrice = statefulService2.order("userB", 20000);
 
         //ThreadA: 사용자A 주문 금액 조회
-        int price = statefulService1.getPrice();    //10000원을 예상
-        System.out.println("price = " + price);
+  //      int price = statefulService1.getPrice();    //10000원을 예상
+        System.out.println("userAprice = " + userAPrice);
 
-        assertThat(statefulService1.getPrice()).isEqualTo(20000);   //B사용자가 나중에 20000원주문해서 그걸로 덮어씌워짐
+        //  assertThat(statefulService1.getPrice()).isEqualTo(10000);   //B사용자가 나중에 20000원주문해서 그걸로 덮어씌워짐
                                                                     //statefulService1과2가 싱글톤패턴으로 같은객체이기 때문
     }
 
